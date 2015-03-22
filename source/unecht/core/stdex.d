27397eaaf9a52a969b6c3bbe0e1168445c67532d
Stephan Dilly <dilly.stephan@gmail.com>
2015-03-22 13:19:44 +0100

    add unitest and cleanup imports

--- source/unecht/core/stdex.d
+++ source/unecht/core/stdex.d
@@ -1,15 +1,19 @@
 ﻿module unecht.core.stdex;
 
-import std.algorithm;
-
 //TODO: allow unstable remove
 ///
 auto removeElement(R,N)(R _haystack, N _needle)
 {
-	auto index = _haystack.countUntil(_needle);
-	
-	if(index != -1)
-		return _haystack.remove(index);
+	import std.algorithm:countUntil,remove;
 
-	return _haystack;
+	auto index = _haystack.countUntil(_needle);
+
+	return (index != -1) ? _haystack.remove(index) : _haystack;
+}
+
+unittest
+{
+	auto arr = [1,5,10];
+	arr = arr.removeElement(5);
+	assert(arr == [1,10]);
 }
