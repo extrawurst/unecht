57bbfa6764c4b27af0ffbb952ce2cff168129986
Stephan Dilly <dilly.stephan@gmail.com>
2015-04-10 01:01:56 +0200

    add hello world sample

---
+++ samples/helloworld/source/app.d
@@ -0,0 +1,59 @@
+module app;
+
+import unecht;
+
+///
+final class TestLogic : UEComponent
+{
+    mixin(UERegisterComponent!());
+
+    override void onCreate() {
+        super.onCreate;
+
+        registerEvent(UEEventType.key, &OnKeyEvent);
+
+        spawnBall();
+    }
+
+    void OnKeyEvent(UEEvent _ev)
+    {
+        if(_ev.keyEvent.action == UEEvent.KeyEvent.Action.Down)
+        {
+            if(_ev.keyEvent.key == UEKey.esc)
+                ue.application.terminate();
+
+            if(_ev.keyEvent.key == UEKey.num2)
+                spawnBall();
+        }
+    }
+
+    void spawnBall()
+    {
+        auto newE = UEEntity.create("obj");
+        import std.random:uniform;
+        newE.sceneNode.position = vec3(uniform(0.0f,1),15,uniform(0.0f,1));
+        newE.addComponent!UEPhysicsBody;
+        newE.addComponent!UEPhysicsColliderBox;
+        newE.addComponent!UEShapeBox;
+    }
+}
+
+shared static this()
+{
+	ue.windowSettings.size.width = 1024;
+	ue.windowSettings.size.height = 768;
+	ue.windowSettings.title = "unecht - hello world sample";
+
+	ue.hookStartup = () {
+		auto newE = UEEntity.create("game");
+        newE.addComponent!TestLogic;
+        newE.addComponent!UEPhysicsColliderPlane;
+
+		auto newE2 = UEEntity.create("camera entity");
+		newE2.sceneNode.position = vec3(0,15,-20);
+        newE2.sceneNode.angles = vec3(30,0,0);
+
+        import unecht.core.components.camera;
+		auto cam = newE2.addComponent!UECamera;
+	};
+}
