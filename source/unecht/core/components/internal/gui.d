d6730f3197291af1a43c757ecde7e98c2ae27fea
Stephan Dilly <dilly.stephan@gmail.com>
2015-04-13 20:40:01 +0200

    update cimgui bindings

--- source/unecht/core/components/internal/gui.d
+++ source/unecht/core/components/internal/gui.d
@@ -31,26 +31,24 @@ public:
 
         ImGuiIO* io = ig_GetIO();
 
-        with(ImGuiKey_){
-            io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;                 // Keyboard mapping. ImGui will use those indices to peek into the io.KeyDown[] array.
-            io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
-            io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
-            io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
-            io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
-            io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
-            io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
-            io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
-            io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
-            io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
-            io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
-            io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
-            io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
-            io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
-            io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
-            io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
-            io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
-        }
-        
+        io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;                 // Keyboard mapping. ImGui will use those indices to peek into the io.KeyDown[] array.
+        io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
+        io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
+        io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
+        io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
+        io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
+        io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
+        io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
+        io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
+        io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
+        io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
+        io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
+        io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
+        io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
+        io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
+        io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
+        io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
+              
         io.RenderDrawListsFn = &renderDrawLists;
     }
 
