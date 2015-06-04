7cb2ced40416b5f10181dabad13cb4b75acba38b
Stephan Dilly <dilly.stephan@gmail.com>
2015-06-04 02:05:29 +0200

    support showing plot of frametimes

--- source/unecht/core/profiler.d
+++ source/unecht/core/profiler.d
@@ -1,11 +1,39 @@
 ﻿module unecht.core.profiler;
 
-version(UEProfiling)
+version(UEProfiling):
+
+
+public import tharsis.prof;
+
+Profiler profiler;
+
+// Get 2 MB more than the minimum (maxEventBytes). Could also use malloc() here.
+ubyte[] storage = new ubyte[Profiler.maxEventBytes + 1024 * 1024 * 2];
+
+static struct UEProfiling
 {
-    public import tharsis.prof;
-    
-    Profiler profiler;
-    
-    // Get 2 MB more than the minimum (maxEventBytes). Could also use malloc() here.
-    ubyte[] storage = new ubyte[Profiler.maxEventBytes + 1024 * 1024 * 2];
+    import std.datetime;
+
+    static float[128] frameTimes;
+
+    static int frameIdx;
+
+    static addFrametime(TickDuration d)
+    {
+        long durUSecs = d.usecs;
+
+        import std.stdio;
+        //writefln("%s",durUSecs);
+
+        if(frameIdx < frameTimes.length)
+            frameTimes[frameIdx++] = durUSecs;
+        else
+        {
+            import core.stdc.string:memmove;
+            memmove(frameTimes.ptr, frameTimes.ptr+1, (frameTimes.length-1)*float.sizeof);
+            frameTimes[frameTimes.length-1] = durUSecs;
+        }
+
+        //writefln("%s",frameTimes);
+    }
 }
