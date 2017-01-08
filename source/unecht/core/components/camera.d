20c0651a2714ee76d3fb30b3226fdb4e62dde407
Stephan Dilly <dilly.stephan@gmail.com>
2017-01-08 04:03:16 +0100

    more docs

--- source/unecht/core/components/camera.d
+++ source/unecht/core/components/camera.d
@@ -17,145 +17,150 @@ version(UEIncludeEditor)
 @EditorInspector("UECamera")
 static class UECameraInspector : IComponentEditor
 {
-    override bool render(UEObject _component)
-    {
-        auto thisT = cast(UECamera)_component;
-        
-        import derelict.imgui.imgui;
-        import unecht.core.components.internal.gui;
-        import std.format;
+	/// render custom inspector for UECamera
+	override bool render(UEObject _component)
+	{
+		auto thisT = cast(UECamera)_component;
 
-        igColorEdit4("clearColor",thisT.clearColor.vector,true);
-        UEGui.DragFloat("fov",thisT.fieldOfView,1,360);
-        UEGui.DragFloat("near",thisT.clipNear,0.01f);
-        UEGui.DragFloat("far",thisT.clipFar,0.01f);
+		import derelict.imgui.imgui;
+		import unecht.core.components.internal.gui;
+		import std.format;
 
-        igCheckbox("isOrthographic",&thisT.isOrthographic);
-        if(thisT.isOrthographic)
-        {
-            UEGui.DragFloat("orthoSize",thisT.orthoSize,0.01f);
-        }
+		igColorEdit4("clearColor",thisT.clearColor.vector,true);
+		UEGui.DragFloat("fov",thisT.fieldOfView,1,360);
+		UEGui.DragFloat("near",thisT.clipNear,0.01f);
+		UEGui.DragFloat("far",thisT.clipFar,0.01f);
 
-        //TODO: impl
-        return false;
-    }
+		igCheckbox("isOrthographic",&thisT.isOrthographic);
+		if(thisT.isOrthographic)
+		{
+			UEGui.DragFloat("orthoSize",thisT.orthoSize,0.01f);
+		}
 
-    mixin UERegisterInspector!UECameraInspector;
+		//TODO: impl
+		return false;
+	}
+
+	mixin UERegisterInspector!UECameraInspector;
 }
 
 //TODO: add properties and make matrix updates implicit
-/// 
-//@CustomInspector!UECameraInspector //TODO: impl
+/// camera component - handles rendering the world through its perspective
 final class UECamera : UEComponent
 {
 	mixin(UERegisterObject!());
 
-    ///
-    @property auto projectionLook() const { return matProjection * matLook; }
+	/// return projection matrix multiplied by look matrix
+	@property auto projectionLook() const { return matProjection * matLook; }
 
-    @Serialize{
-        float fieldOfView = 60;
-        float clipNear = 1;
-        float clipFar = 1000;
-        
-        vec4 clearColor = vec4(0,0,0,1);
-        bool clearBitColor = true;
-        bool clearBitDepth = true;
-        int visibleLayers = UECameraDefaultLayers;
-        
-        bool isOrthographic=false;
-        float orthoSize=1;
-        
-        UERect viewport;
-    }
+	@Serialize{
+		float fieldOfView = 60;
+		float clipNear = 1;
+		float clipFar = 1000;
 
-    ray screenToRay(vec2 screenPos)
-    {
-        import unecht.ue:ue;
+		vec4 clearColor = vec4(0,0,0,1);
+		bool clearBitColor = true;
+		bool clearBitDepth = true;
+		int visibleLayers = UECameraDefaultLayers;
 
-        UESize viewportSize = UESize(
-            cast(int)(viewport.size.x * ue.application.windowSize.width),
-            cast(int)(viewport.size.y * ue.application.windowSize.height));
+		bool isOrthographic=false;
+		float orthoSize=1;
 
-        float x = (2.0f * screenPos.x) / viewportSize.width - 1.0f;
-        float y = (2.0f * screenPos.y) / viewportSize.height - 1.0f;
-
-        auto mouseClip = vec4 (x, -y, 1, 1);
-
-        auto matUnproj = matProjection.inverse();
-
-        auto mouseWorld = matUnproj * mouseClip;
-
-        mouseWorld /= (mouseWorld.w);
-
-        auto dir = mouseWorld.xyz;
-
-        dir.normalize();
-
-        dir = dir.xyz * matLook.get_rotation();
-
-        return ray(entity.sceneNode.position, dir);
-    }
-
-	void updateLook()
-	{
-        auto lookat = entity.sceneNode.position + entity.sceneNode.forward;
-
-        matLook = mat4.look_at(entity.sceneNode.position, lookat, entity.sceneNode.up);
+		UERect viewport;
 	}
 
-	void updateProjection()
+	///
+	public ray screenToRay(vec2 screenPos)
 	{
-        if(!isOrthographic)
+		import unecht.ue:ue;
+
+		UESize viewportSize = UESize(
+			cast(int)(viewport.size.x * ue.application.windowSize.width),
+			cast(int)(viewport.size.y * ue.application.windowSize.height));
+
+		float x = (2.0f * screenPos.x) / viewportSize.width - 1.0f;
+		float y = (2.0f * screenPos.y) / viewportSize.height - 1.0f;
+
+		auto mouseClip = vec4 (x, -y, 1, 1);
+
+		auto matUnproj = matProjection.inverse();
+
+		auto mouseWorld = matUnproj * mouseClip;
+
+		mouseWorld /= (mouseWorld.w);
+
+		auto dir = mouseWorld.xyz;
+
+		dir.normalize();
+
+		dir = dir.xyz * matLook.get_rotation();
+
+		return ray(entity.sceneNode.position, dir);
+	}
+
+	///
+	private void updateLook()
+	{
+		auto lookat = entity.sceneNode.position + entity.sceneNode.forward;
+
+		matLook = mat4.look_at(entity.sceneNode.position, lookat, entity.sceneNode.up);
+	}
+
+	///
+	private void updateProjection()
+	{
+		if(!isOrthographic)
 		{
 			import unecht.ue:ue;
 			auto w = ue.application.framebufferSize.width;
 			auto h = ue.application.framebufferSize.height;
-		    matProjection = mat4.perspective(w, h, fieldOfView, clipNear, clipFar);
+			matProjection = mat4.perspective(w, h, fieldOfView, clipNear, clipFar);
 		}
-        else
-        {
-            matProjection = mat4.orthographic(-(orthoSize/2),(orthoSize/2),-(orthoSize/2),(orthoSize/2),clipNear,clipFar);
-        }
+		else
+		{
+			matProjection = mat4.orthographic(-(orthoSize/2),(orthoSize/2),-(orthoSize/2),(orthoSize/2),clipNear,clipFar);
+		}
 	}
 
-	void render()
+	/// render all renderables through the perspective of this camera
+	///TODO: note: needs to be moved aways
+	public void render()
 	{
-        import unecht.ue:ue;
-        import derelict.opengl3.gl3;
-		
+		import unecht.ue:ue;
+		import derelict.opengl3.gl3;
+
 		updateProjection();
 		updateLook();
-		
+
 		int clearBits = 0;
 		if(clearBitColor) clearBits |= GL_COLOR_BUFFER_BIT;
 		if(clearBitDepth) clearBits |= GL_DEPTH_BUFFER_BIT;
-		
-        if(clearBits!=0)
-        {
-    		glClearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
-    		glClear(clearBits);
-        }
+
+		if(clearBits!=0)
+		{
+			glClearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
+			glClear(clearBits);
+		}
 
 		UESize viewportSize = UESize(
 			cast(int)(viewport.size.x * ue.application.framebufferSize.width),
 			cast(int)(viewport.size.y * ue.application.framebufferSize.height));
 		glViewport(viewport.pos.left,viewport.pos.top,viewportSize.width,viewportSize.height);
 
-        auto renderers = ue.scene.gatherAllComponents!UERenderer;
-		
+		auto renderers = ue.scene.gatherAllComponents!UERenderer;
+
 		foreach(r; renderers)
 		{
 			if(r.enabled && r.sceneNode.enabled)
-            {
-                import unecht.core.stdex;
-                if(testBit(visibleLayers, r.entity.layer))
-				    r.render(this);
-            }
+			{
+				import unecht.core.stdex;
+				if(testBit(visibleLayers, r.entity.layer))
+					r.render(this);
+			}
 		}
 	}
 
 private:
-    mat4 matProjection = mat4.identity;
-    mat4 matLook = mat4.identity;
+	mat4 matProjection = mat4.identity;
+	mat4 matLook = mat4.identity;
 }
